9014a5dd2774b6fa28615e13be3569c680bbfd55
Lenovo <Lenovo@DESKTOP-067GH63>
2022-01-28 10:43:02 +0330

    ignore some files

---
+++ EF_ADO_Benchmarking.App/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EF_ADO_Benchmarking.App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
